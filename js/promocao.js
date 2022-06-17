59309cff716a6d8aadb4cc74885b5fa68a80249b
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-06-17 10:26:54 -0300

    Ajustes

--- js/promocao.js
+++ js/promocao.js
@@ -1,4 +1,16 @@
 const btnMenuPromo = document.querySelectorAll(".js-btn-menu");
 const conteudoPromo = document.querySelectorAll(".js-conteudo-promo");
+conteudoPromo[0].classList.add("show")
 
-console.log(conteudoPromo)
+const ativandoTab = (index) => {
+    conteudoPromo.forEach((div) => {
+        div.classList.remove("show");
+    });
+    conteudoPromo[index].classList.add("show");
+}
+
+btnMenuPromo.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+        ativandoTab(index);
+    });
+});
