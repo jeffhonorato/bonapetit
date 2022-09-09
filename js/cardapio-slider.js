c594d4ceb8bd3cefa1f341eba111b9ae8909997e
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-09-09 10:50:58 -0300

    ajustes

--- js/cardapio-slider.js
+++ js/cardapio-slider.js
@@ -14,4 +14,8 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
     },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
   });
