e24a1ad3662cc9c49b56dd73cba5d6dc44634c20
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-09-09 14:25:59 -0300

    Ajustes

--- js/cardapio-slider.js
+++ js/cardapio-slider.js
@@ -41,3 +41,47 @@ var swiper = new Swiper(".pizza-swiper", {
         prevEl: ".swiper-button-prev",
       },
   });
+
+  var swiper = new Swiper(".massa-swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 60,
+      modifier: 3,
+      slideShadows: false,
+    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  var swiper = new Swiper(".bebida-swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 60,
+      modifier: 3,
+      slideShadows: false,
+    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
