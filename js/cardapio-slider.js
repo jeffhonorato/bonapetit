5a89a075b3eaaa0ac1df80e99327aebc25718b0c
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-09-09 12:52:33 -0300

    Ajustes

--- js/cardapio-slider.js
+++ js/cardapio-slider.js
@@ -19,3 +19,25 @@ var swiper = new Swiper(".pizza-swiper", {
         prevEl: ".swiper-button-prev",
       },
   });
+
+  var swiper = new Swiper(".carne-swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 60,
+      modifier: 3,
+      slideShadows: false,
+    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
