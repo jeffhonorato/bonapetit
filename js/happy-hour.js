471d9c8388b13b61458a7ae39335a5f482326690
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-10-24 07:35:21 -0300

    Ajustes

--- js/happy-hour.js
+++ js/happy-hour.js
@@ -1,15 +1,19 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".musica", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: "1",
+    loop: "true",
     coverflowEffect: {
       rotate: 10,
       stretch: 0,
       depth: 60,
-      modifier: 7,
+      modifier: 9,
       slideShadows: false,
     },
+    pagination: {
+        el: ".swiper-pagination",
+      },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
